60b1972d6cb3734ecb1e3a1785c154c7fba1c2b6
Fabs02 <fabriciocalvanese@gmail.com>
2024-11-17 20:15:02 -0300

    First commit

---
+++ exercicios.py
@@ -0,0 +1,37 @@
+# 1.1 Hello World
+print("Hello World!!")
+
+
+# 1.2 Extremamente básico
+A = 10
+B = 9
+
+x = A + B
+print ("X =", x)
+
+
+# 1.3 Area do circulo
+pi = 3.14159
+raio = input("Digite o raio do circulo: ")
+
+area = pi * (raio * 2)
+
+print("A =", area)
+
+
+# 1.4 Soma simples
+A = input("Digite o primeiro valor:")
+B = input("Digite o segundo valor: ")  
+
+soma = A + B
+
+print("Soma =", soma)
+
+
+# 1.5 Produto simples
+A = input("Digite o primeiro valor: ")
+B = input("Digite o segundo valor: ")
+
+produto = A * B
+
+print("Produto =", produto)
